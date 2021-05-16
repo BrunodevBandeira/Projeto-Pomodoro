4b7c89492b4c7ef1a1c5bf13e6ff563fb6f15fc1
Bruno Bandeira <brunodev29ab@gmail.com>
2021-05-16 15:09:15 -0300

    Iniciando com o JS

---
+++ script.js
@@ -0,0 +1,8 @@
+let acao = document.getElementoById('acao') //Estou pegando o input da ação
+let pausa = document.getElementById("pausa")        //
+let sessoes = docoment.getElementById("sessoes")    //
+let segundos
+
+var bell = new Audio("./audio/bell.mp3")
+var volta = new Audio("./audio/volta.mp3")
+var final = new Audio("./audio/final.mp3")
