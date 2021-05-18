4008831826a4b3583842384b3d9b03786a7aa164
Bruno Bandeira <brunodev29ab@gmail.com>
2021-05-17 23:53:45 -0300

    Timer 80% finalizado

--- script.js
+++ script.js
@@ -54,11 +54,11 @@ function iniciar() {
          document.getElementById("title_sessao").innerHTML = sessoes_valor + "sessão restante"
       }
 
-      let title =  document.getElementById("title")
+      let title = document.getElementById("title")
       title.innerHTML = "AÇÂO"
       title.style.fontSize = "25pt"
       title.style.fontWeight = "bold"
-      title.style.setProperty("color", "#28a745" , "important")
+      title.style.setProperty("color", "#28a745", "important")
 
       min = Number(localStorage.getItem("acao"))
 
@@ -79,27 +79,87 @@ function iniciar() {
       function segTimer() {
          segundos = segundos - 1
          document.getElementById("minutes_ok").innerHTML = segundos
+
+
+
+         if (segundos <= 0) {
+            if (min <= 0) {
+               clearInterval(min_interval)
+               clearInterval(seg_interval)
+
+               bell.play()
+               momentoPausa()
+            }
+            segundos = 60
+         }
+
       }
 
+   }
+   function momentoPausa() {
 
-      if(segundos <= 0){
-         if(min <= 0){
+      let title = document.getElementById("title")
+      title.innerHTML = "PAUSA"
+      title.style.fontSize = "25pt"
+      title.style.fontWeight = "bold"
+      title.style.setProperty("color", "#dc3545", "important")
+
+
+      min_pausa = Number(localStorage.getItem("pausa"))
+
+
+      min_pausa = min_pausa - 1
+      segundos = 59
+
+
+      document.getElementById("minutos_ok").innerHTML = min_pausa
+      document.getElementById("seconds_ok").innerHTML = segundos
+
+
+
+      var min_interval = setInterval(minTimer, 60000)
+      var seg_interval = setInterval(segTimer, 1000)
+
+
+      function minTimer() {
+         min_pausa = min_pausa - 1
+         document.getElementById("minutos_ok").innerHTML = min_pausa
+      }
+   }
+
+   function segTimer() {
+      segundos = segundos - 1
+      document.getElementById("minutes_ok").innerHTML = segundos
+
+      if (segundos <= 0) {
+         if (min_pausa <= 0) {
+            ses = Number(localStorage.getItem("sessoes"))
+            ses = ses - 1
+
+            localStorage.setItem("sessoes", String(ses))
+
+
             clearInterval(min_interval)
             clearInterval(seg_interval)
 
-            bell.play()
-            momentoPausa()
-         }
+            if(ses <= 0 ){
+               final.play()
+               localStorage.clear()
+
+               document.getElementById("config").style.setProperty("display", "none", "important")
+               document.getElementById("timer").style.setProperty("display", "none", "important")
+               document.getElementById("fim").style.setProperty("display", "block", "important")
+
+            } else {
+               volta.play()
+            }
+
+          }
          segundos = 60
       }
-
-   }
-
-
-   function momentoPausa() {
-
    }
 }
+
 
 
 
